830a7d91175bf9b606e25f27a18392888d3bf4b8
KRISHNADEV SHARMA <krishnadevsharma79248@gmail.com>
2026-06-30 18:50:58 +0530

    Time: 5 ms (53%), Space: 9.9 MB (47.33%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.cpp
+++ 0686-repeated-string-match/0686-repeated-string-match.cpp
@@ -1,35 +1,20 @@
 class Solution {
 public:
-    bool check(string& a, string& b) {
-        int m = a.size();
-        int n = b.size();
-        for (int i = 0; i <= m - n; i++) {
-            int j;
-            for (j = 0; j < n; j++) {
-                if (a[i + j] != b[j])
-                    break;
-            }
-            if (j == n)
-                return true;
-        }
-        return false;
-    }
     int repeatedStringMatch(string a, string b) {
-        int m = a.size();
-        int n = b.size();
-        int ans = 1;
         string temp = a;
-        while (a.size() < b.size()) {
-            a += temp;
-            ans++;
+        int count = 1;
+        while(temp.size() < b.size()){
+            temp += a;
+            count++;
         }
-        if (check(a, b)) {
-            return ans;
-        }
-        a += temp;
-        if (check(a, b)) {
-            return ans + 1;
-        }
+        if(temp.find(b) != string::npos)
+        return count;
+
+        temp += a;
+
+        if(temp.find(b) != string::npos)
+        return count + 1;
+
         return -1;
     }
 };
