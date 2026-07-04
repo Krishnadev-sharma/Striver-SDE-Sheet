25e694854794c5d6ddcc55aa5af87a2047b1ee38
KRISHNADEV SHARMA <krishnadevsharma79248@gmail.com>
2026-07-04 21:06:21 +0530

    Time: 0 ms (100%), Space: 11 MB (42.61%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -12,6 +12,38 @@
 class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
+        // Morris approach
+        vector<int>ans;
+        TreeNode* curr = root;
+        while(curr){
+            if(curr->left == NULL){
+                ans.push_back(curr->val);
+                curr = curr->right;
+            }
+            else{
+                TreeNode* prev = curr->left;
+                while(prev->right && prev->right != curr){
+                    prev = prev->right;
+                }
+                if(prev->right == NULL){
+                    ans.push_back(curr->val);
+                    prev->right = curr;
+                    curr = curr->left;
+                }
+                else{
+                    prev->right = NULL;
+                    curr = curr->right;
+                }
+            }
+        }
+        return ans;
+
+
+
+
+        /*
+        iterrative solution
+
         vector<int>ans;
         if(root == NULL){
             return ans;
@@ -29,6 +61,6 @@ public:
                 st.push(node->left);
             }
         }
-        return ans;
+        return ans; */
     }
 };
