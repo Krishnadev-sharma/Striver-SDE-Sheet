1f612925842e72a462aed81732a72a2860d84901
KRISHNADEV SHARMA <krishnadevsharma79248@gmail.com>
2026-08-10 01:52:12 +0530

    Time: 7 ms (77.22%), Space: 13.5 MB (67.28%) - LeetHub

---
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int stoneGameII(vector<int>& piles) {
+        int n = piles.size();
+        vector<vector<int>>dp(n, vector<int>(n+1,0));
+        vector<int>s(n,0);
+        s[n-1]=piles[n-1];
+        for(int i = n-2; i>=0; i--){
+            s[i] = s[i+1] + piles[i];
+        }
+        for(int i = n-1; i >=0; i--){
+            for(int j = 1; j <= n; j++){
+                if(i + 2 * j >= n){
+                    dp[i][j] = s[i];
+                }
+                else{
+                    for(int x = 1; x <= 2*j; x++){
+                        dp[i][j] = max(dp[i][j], s[i] - dp[i + x][max(j, x)]);
+                    }
+                }
+            }
+        }
+        return dp[0][1];
+    }
+};
